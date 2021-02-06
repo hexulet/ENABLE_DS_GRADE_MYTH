7fcec82516d6cb8669bb22c41cdb41ae6159c8f8
iMerv3 <67241171+iMerv3@users.noreply.github.com>
2021-02-06 05:11:29 +0100

    Update DragonSoul.cpp

--- .srcs/server/game/src/DragonSoul.cpp
+++ .srcs/server/game/src/DragonSoul.cpp
@@ -2,7 +2,7 @@
 	return col_type * DRAGON_SOUL_STEP_MAX * DRAGON_SOUL_BOX_SIZE + row_type * DRAGON_SOUL_BOX_SIZE;
 // Replace with:
 #ifdef ENABLE_DS_GRADE_MYTH
-	return col_type * DRAGON_SOUL_GRADE_MAX * DRAGON_SOUL_BOX_SIZE + (row_type * DRAGON_SOUL_BOX_SIZE);
+	return col_type * DRAGON_SOUL_GRADE_MAX * DRAGON_SOUL_BOX_SIZE + row_type * DRAGON_SOUL_BOX_SIZE;
 #else
 	return col_type * DRAGON_SOUL_STEP_MAX * DRAGON_SOUL_BOX_SIZE + row_type * DRAGON_SOUL_BOX_SIZE;
 #endif
