02a777b58c200e31a6933cbb73ac6553a5bde092
iMerv3 <67241171+iMerv3@users.noreply.github.com>
2021-02-07 20:46:07 +0100

    v1.1
    Fixed the load of RefineStepTables (wrong size).

--- .srcs/server/game/src/dragon_soul_table.cpp
+++ .srcs/server/game/src/dragon_soul_table.cpp
@@ -4,3 +4,19 @@
 #ifdef ENABLE_DS_GRADE_MYTH
 	"grade_myth",
 #endif
+
+
+
+// Search:
+				if (DRAGON_SOUL_GRADE_MAX != vec_probs.size())
+				{
+					sys_err ("In %s group of RefineStepTables, probability list size is not %d.",
+						m_vecDragonSoulNames[i].c_str(), DRAGON_SOUL_GRADE_MAX);
+					return false;
+				}
+// Replace with:
+				if (DRAGON_SOUL_STEP_MAX != vec_probs.size())
+				{
+					sys_err ("In %s group of RefineStepTables, probability list size is not %d.", m_vecDragonSoulNames[i].c_str(), DRAGON_SOUL_STEP_MAX);
+					return false;
+				}
